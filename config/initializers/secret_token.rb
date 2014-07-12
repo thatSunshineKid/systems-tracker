388a4ff85a23a58e605406ab61a4207f9094eac8
Adam <adam@csuac.com>
2014-07-13 01:47:17 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SystemsTracker::Application.config.secret_key_base = '3ea774facfe6cdcf52707b64b9daca7b8f2dd4f63bef542d71cba1a614da00e499ef304c486c3473b31e64fe1022e757bc4e5dd79f28e90df00d6aa91fad766c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SystemsTracker::Application.config.session_store :cookie_store, key: '_systems_tracker_session'
